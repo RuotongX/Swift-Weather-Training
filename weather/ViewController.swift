a4d888958951b22b7a091baba2b59fb431c2c8de
RuotongX <bagadiyi@gmail.com>
2019-03-06 04:21:18 +1300

    Initial Commit

---
+++ weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by RuotongX on 2019/3/6.
+//  Copyright © 2019 RuotongX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
