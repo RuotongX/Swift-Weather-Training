27e07fec5aadd977e017bd590d6afb46029e397b
RuotongX <bagadiyi@gmail.com>
2019-03-24 21:30:49 +1300

    Trying to add a table view which display the weather forcast.
    Meet some trouble while connect the table to view controller class, need to learn some guide.:(

---
+++ weather/TableCellTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TableCellTableViewCell.swift
+//  weather
+//
+//  Created by RuotongX on 2019/3/24.
+//  Copyright © 2019 RuotongX. All rights reserved.
+//
+
+import UIKit
+
+class TableCellTableViewCell{
+    @IBOutlet weak var WeatherImage: UIImageView!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+}
